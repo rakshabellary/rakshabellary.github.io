50be62b4fc371d4f0650237ea2d7a0720d0a0613
r-bellary <rakshabellary@gmail.com>
2023-11-01 03:46:30 -0400

    added new html and js files

---
+++ projects.js
@@ -0,0 +1,9 @@
+document.getElementById("software-eng-button").addEventListener("click", function () {
+    var contentContainer = document.getElementById("content-container");
+    contentContainer.innerHTML = "Software Engineering content goes here.";
+});
+
+document.getElementById("quest-button").addEventListener("click", function () {
+    var contentContainer = document.getElementById("content-container");
+    contentContainer.innerHTML = "QUEST Honors content goes here.";
+});
