fe54b81c700cd5cf8f7c588a9ed75309f394f95d
r-bellary <rakshabellary@gmail.com>
2023-11-01 01:56:44 -0400

    updated javascript to make text visible

--- index.8464b05de136ea6c9f71.js
+++ index.8464b05de136ea6c9f71.js
@@ -2,6 +2,7 @@ console.log("Hello from index.js!");
 
 $(document).ready(function () {
     setTimeout(function () {
+        $('.typewriter-delay').css('display', 'inline-block');
         $('html, body').animate({
             scrollTop: $('#second').offset().top
         }, 300);
