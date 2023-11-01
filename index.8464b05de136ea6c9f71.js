032dba9b448b5b23e835dabf8f0293f4f6130b02
r-bellary <rakshabellary@gmail.com>
2023-10-31 22:57:47 -0400

    added window automatic scroll

--- index.8464b05de136ea6c9f71.js
+++ index.8464b05de136ea6c9f71.js
@@ -1,1 +1,8 @@
 console.log("Hello from index.js!");
+
+$(window).load(function () {
+    $('body').delay(5000)
+        .animate({
+            'scrollTop': $('#second').offset().top
+        }, 300);
+});
