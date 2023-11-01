7423a0a9c10db3bdafdb151abd8eb5dc4abd6d22
r-bellary <rakshabellary@gmail.com>
2023-11-01 02:04:20 -0400

    changed timers

--- index.8464b05de136ea6c9f71.js
+++ index.8464b05de136ea6c9f71.js
@@ -2,9 +2,11 @@ console.log("Hello from index.js!");
 
 $(document).ready(function () {
     setTimeout(function () {
-        $('.typewriter-delay').css('display', 'inline-block');
+      $('.typewriter-delay').css('display', 'inline-block');
+      setTimeout(function () {
         $('html, body').animate({
-            scrollTop: $('#second').offset().top
+          scrollTop: $('#second').offset().top
         }, 300);
-    }, 15000);
-});
+      }, 3000);
+    }, 7000);
+  });
